b8bc82474c0d871f367e264a095a29ad8809577f
Deimonides <deimonides@gmail.com>
2022-05-06 01:55:57 -0300

    Desafio 2b

--- Desafios_Entregables/desafio_02.js
+++ Desafios_Entregables/desafio_02.js
@@ -2,75 +2,77 @@ const fs = require('fs');
 class Contenedor {
 
     constructor(archivo) {
-        this.archivo = archivo;
+        this.archivo = './'+archivo;
     }
     
     async getAll() { 
-        
         try {
             let archivo = this.archivo;
-            let arrProductos = [];
             const strProductos = await fs.promises.readFile( archivo, 'utf-8' )
-            //console.log(`getAll:contenido = `, contenido);
+            const arrProductos = JSON.parse(strProductos);
+            return arrProductos
         }
         catch (error) {
-            console.log(error.message)
+            console.error(error.message)
         }
-    } //listo
+    }
     
     async save(arrNuevoProducto){
-        console.log(`Guardar producto: `, arrNuevoProducto);
         let archivo = this.archivo;
-            //console.log(`save:archivo = `,archivo);
         let arrProductos = [];
-        // conseguir id libre
         let strProductos = await fs.promises.readFile( archivo, 'utf-8' );
-            //console.log(`strProductos leidos: `,strProductos);
         strProductos &&  (arrProductos = JSON.parse(strProductos))
-            //console.log(`arrProductos leidos: `,arrProductos);
-        
         let nuevoId = 1;
         arrProductos && (nuevoId = arrProductos.length + 1);
-        //console.log(`nuevoId: `, nuevoId);
-        
         arrNuevoProducto.id = nuevoId;
-        //console.log(`arrNuevoProducto: `, arrNuevoProducto);
         arrProductos = arrProductos.concat(arrNuevoProducto)
-        //console.log('*-*-*');
         strProductos = JSON.stringify(arrProductos, null, 2);
-        console.log(`strProductos = `, strProductos);
-        //console.log(`**nuevoId: `, nuevoId);
         
         // GRABAR TODOS LOS PRODUCTOS
         await fs.promises.writeFile(archivo, strProductos) 
-       
-       
-        
         console.log(`El nuevo producto ${arrNuevoProducto.title} se grabó con el id `, nuevoId);       
         return nuevoId;
     }
     
     async getById(idNum){
-        let archivo = this.archivo;
-        let arrProductos = [];
-        let strProductos = await fs.promises.readFile( archivo, 'utf-8' );
-        arrProductos = JSON.parse(strProductos)
-        console.log(`Mostrar el producto con ID:`,idNum);
-        // array.find
+        try {
+            let archivo = this.archivo;
+            let strProductos = await fs.promises.readFile( archivo, 'utf-8' );
+            const arrProductos = JSON.parse(strProductos)
+            let producto = arrProductos.find( item => item.id === idNum );
+            if (producto) {
+                return producto 
+            } else {
+                producto = '! Error: producto no encontrado'
+                return producto
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
     }
     
-    deleteById(idNum){
-        console.log(`Eliminar producto con ID:`, idNum);
-        // array.filter
+    async deleteById(idNum){
+        let archivo = this.archivo;
+        const strProductos = await fs.promises.readFile( archivo, 'utf-8' )
+        const arrProductos = JSON.parse(strProductos);
+        const existe = arrProductos.find(item => item.id === idNum)
+        if (existe) {
+            const arrFiltrado = arrProductos.filter(item => item.id != idNum)
+            const strFiltrado = JSON.stringify(arrFiltrado, null, 2);
+            await fs.promises.writeFile(archivo, strFiltrado);
+        } else {
+            console.log(`! Error: el producto con id ${idNum} no existe.`)
+        }
     }
     
     // VACIAR ARCHIVO
-    deleteAll() {
-        fs.writeFile ( archivo, "", err => {
+    async deleteAll() {
+        fs.writeFile ( this.archivo, "", err => {
             if (err) {
                 console.error(err);
             } else {
-                console.log('Se vació el archivo.');
+                console.log('>> Se vació el archivo.');
             }
         
         })
@@ -80,31 +82,41 @@ class Contenedor {
 
 
 console.clear();
-console.log( "----------------------------------------------------------------" );
-console.log( "-----------------------------INICIO-----------------------------" );
-console.log( "----------------------------------------------------------------" );
+console.log( "------------------------------------------------" );
+console.log( "---------------------INICIO---------------------" );
+console.log( "------------------------------------------------" );
 
-// crear nuevo archivo
-let dbProductos = new Contenedor ('./Productos.txt')
-console.log( `+ Nuevo archivo creado: `, dbProductos );
-//fs.writeFileSync( this.archivo, "NULL");
-
-//console.log( "----------------------------------------------------------------" );
-const producto1 = { 
-    "title": "amd7",
-    "price": 60000,
-    "thumbnail": "https://i.postimg.cc/90wRTR3D/amd7.png"
-}
-//console.log(`producto1: `,producto1);
-const producto2 = { 
-    "title": "amd9",
-    "price": 90000,
-    "thumbnail": "https://i.postimg.cc/HjGJtYHB/amd9.png"
-}
-//console.log(`producto2: `,producto2);
-
-//console.log( "----------------------------------------------------------------" );
-//console.log( dbProductos.archivo )
-//console.log( `Contenido del archivo: `, dbProductos.getAll( dbProductos.archivo ) );
-dbProductos.save(producto1);
-
+;(async () => {
+    const dbProductos = new Contenedor('Productos.txt')
+    
+    /*const producto = {
+        title: 'Escuadra',
+        price: 123.45,
+        thumbnail: 'https://escuadra.jpg'
+    }*/
+    
+    /* const producto = {
+        title: 'Calculadora',
+        price: 234.56,
+        thumbnail: 'https://calculadora.jpg'
+    } */
+    
+    const producto = {
+        title: 'Globo Terráqueo',
+        price: 345.67,
+        thumbnail: 'https://globoterr.jpg'
+    }
+   
+    await dbProductos.save(producto);
+  
+    const productoId = await dbProductos.getById(4);
+    console.log(`>> Mostrar producto específico: `,productoId);
+  
+  
+    const todosLosProductos = await dbProductos.getAll();
+    console.log(`>> Listar todos los productos: `,todosLosProductos);
+  
+    await dbProductos.deleteById(5);
+  
+    //await dbProductos.deleteAll();
+})()
